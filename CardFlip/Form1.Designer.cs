67750aad8ba1ee9a38ac0034316b8f70d569f3cd
Dalton.miller <Dalton.miller@B228-12>
2026-09-17 20:10:58 -0600

    Created card form layout, and import images

--- CardFlip/Form1.Designer.cs
+++ CardFlip/Form1.Designer.cs
@@ -13,12 +13,85 @@ partial class Form1
 
     private void InitializeComponent()
     {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        picCardF = new PictureBox();
+        picCardB = new PictureBox();
+        btnCardB = new Button();
+        btnCardF = new Button();
+        btnFlip = new Button();
+        ((System.ComponentModel.ISupportInitialize)picCardF).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)picCardB).BeginInit();
         SuspendLayout();
-        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(580, 400);
+        // 
+        // picCardF
+        // 
+        picCardF.Image = (Image)resources.GetObject("picCardF.Image");
+        picCardF.Location = new Point(216, 28);
+        picCardF.Name = "picCardF";
+        picCardF.Size = new Size(150, 210);
+        picCardF.SizeMode = PictureBoxSizeMode.StretchImage;
+        picCardF.TabIndex = 1;
+        picCardF.TabStop = false;
+        picCardF.Visible = false;
+        // 
+        // picCardB
+        // 
+        picCardB.Image = (Image)resources.GetObject("picCardB.Image");
+        picCardB.Location = new Point(25, 28);
+        picCardB.Name = "picCardB";
+        picCardB.Size = new Size(150, 210);
+        picCardB.SizeMode = PictureBoxSizeMode.CenterImage;
+        picCardB.TabIndex = 2;
+        picCardB.TabStop = false;
+        picCardB.Click += picCardB_Click;
+        // 
+        // btnCardB
+        // 
+        btnCardB.Location = new Point(55, 306);
+        btnCardB.Name = "btnCardB";
+        btnCardB.Size = new Size(75, 43);
+        btnCardB.TabIndex = 3;
+        btnCardB.Text = "SHOW BACK";
+        btnCardB.UseVisualStyleBackColor = true;
+        // 
+        // btnCardF
+        // 
+        btnCardF.Location = new Point(245, 306);
+        btnCardF.Name = "btnCardF";
+        btnCardF.Size = new Size(75, 43);
+        btnCardF.TabIndex = 4;
+        btnCardF.Text = "SHOW FACE";
+        btnCardF.UseVisualStyleBackColor = true;
+        // 
+        // btnFlip
+        // 
+        btnFlip.Location = new Point(144, 361);
+        btnFlip.Name = "btnFlip";
+        btnFlip.Size = new Size(83, 27);
+        btnFlip.TabIndex = 5;
+        btnFlip.Text = "FLIP";
+        btnFlip.UseVisualStyleBackColor = true;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(393, 410);
+        Controls.Add(btnFlip);
+        Controls.Add(btnCardF);
+        Controls.Add(btnCardB);
+        Controls.Add(picCardB);
+        Controls.Add(picCardF);
         Name = "Form1";
         Text = "Card Flip";
+        ((System.ComponentModel.ISupportInitialize)picCardF).EndInit();
+        ((System.ComponentModel.ISupportInitialize)picCardB).EndInit();
         ResumeLayout(false);
     }
+
+    private PictureBox picCardF;
+    private PictureBox picCardB;
+    private Button btnCardB;
+    private Button btnCardF;
+    private Button btnFlip;
 }
--- CardFlip/Form1.cs
+++ CardFlip/Form1.cs
@@ -1,9 +1,15 @@
 namespace CardFlip;
+
 public partial class Form1 : Form
 {
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void picCardB_Click(object sender, EventArgs e)
+    {
+
     }
 
     // Follow README.md. Add controls in the Designer and create each
